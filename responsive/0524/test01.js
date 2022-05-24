7fe50835c481c5402b70d3b9e440e21c9012f611
LeeGyuho <andrewgyuholee@gmail.com>
2022-05-24 16:31:20 +0900

    업데이트

---
+++ responsive/0524/test01.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+  document.querySelector('.menu_mobile').classList.toggle('show');
+}
+
+function showMenu() {
+  document.querySelector('.menu_mobile').classList.add('show');
+}
+
+function closeMenu() {
+  document.querySelector('.menu_mobile').classList.remove('show');
+}
